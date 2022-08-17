9211bf51940b1d2fb8fa90e374c9b6bfb3ed4327
GloomyFoodeater <s.paulavets@gmail.com>
2022-08-17 10:50:24 +0300

    Запуск gunicorn с адрессом 0.0.0.0 вместо 127.0.0.1

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -b 127.0.0.1:8080 hello:app
+
+# hello - модуль из текущей директории => cd /home/box/web
+gunicorn -b 0.0.0.0:8080 hello:app
