2c3985a75e32c75388e6727d7a5afd1b81c07edb
GloomyFoodeater <s.paulavets@gmail.com>
2022-08-17 10:36:40 +0300

    Добавлено задание 1.9

---
+++ hello.py
@@ -0,0 +1,8 @@
+def app(environ, start_response):
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    body = '\r\n'.join(environ['QUERY_STRING'].split('&'))
+    start_response(status, headers)
+    return [bytes(body, 'utf-8')]
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn -b 127.0.0.1:8080 hello:app
